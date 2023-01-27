ded478afdafdf5eacd985f77b4e3defdcde96d38
JasonGonzi <123767781+JasonGonzi@users.noreply.github.com>
2023-01-27 13:40:25 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,51 @@
+let Images = [
+images.createImage(`
+    . . # . .
+    . # . # .
+    . # . # .
+    . # . # .
+    . . # . .
+    `),
+images.createImage(`
+    . . # . .
+    . . # . .
+    . . # . .
+    . . # . .
+    . . # . .
+    `),
+images.createImage(`
+    . # # . .
+    . . . # .
+    . . # . .
+    . # . . .
+    . # # # .
+    `),
+images.createImage(`
+    . # # . .
+    . . . # .
+    . # # . .
+    . . . # .
+    . # # . .
+    `),
+images.createImage(`
+    . # . # .
+    . # . # .
+    . # # # .
+    . . . # .
+    . . . # .
+    `),
+images.createImage(`
+    . # # # .
+    . # . . .
+    . # # # .
+    . . . # .
+    . # # # .
+    `)
+]
+let Score = 0
+basic.forever(function () {
+    Images[Score].showImage(0)
+})
+basic.forever(function () {
+	
+})
